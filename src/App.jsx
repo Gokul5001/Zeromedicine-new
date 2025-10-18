02932abaacfd1b03671962b6bdc0c6cab56d19a7
Gokul5001 <Gokul5001>
2025-10-18 14:43:48 +0530

    button updation

--- src/App.jsx
+++ src/App.jsx
@@ -482,9 +482,13 @@ const LandingPage = () => {
     © 2025 Zeromedixine. All rights reserved.
   </div>
 </footer>
-<button className="fixed bottom-6 right-6 bg-gradient-to-r from-blue-600 to-green-500 text-white p-4 rounded-full shadow-xl hover:scale-110 transition-transform duration-300">
-      <MessageCircle className="w-8 h-8" />
-    </button>
+{/* Floating WhatsApp Button */}
+<button
+  className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition z-50"
+>
+  <MessageCircle className="w-6 h-6" />
+</button>
+
     </div>
   );
 };
